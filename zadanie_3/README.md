013e1d4e744104ce562495e432f63948d3689a5d
Redellex <masterpaw5679966@gmail.com>
2015-12-27 20:51:34 +0100

    Added zadanie_3.

---
+++ zadanie_3/README.md
@@ -0,0 +1,13 @@
+#Spis tresci:
+    a) Algorytmy:
+        -Shell sort
+        -Bubble sort
+        -Quick sort
+    b) Pliki testujace:
+        -Shell_sort_spec.rb
+        -Bubble_sort_spec.rb
+        -Quick_sort_spec.rb
+    c) Pliki bin
+        -Benchmark
+
+Aby uruchomic testy wystarczy w katalogu glownym wpisac rspec.
---
+++ zadanie_3/lib/main.rb
@@ -0,0 +1,49 @@
+
+def Menu1()
+    print ">  Dodaj  <\n"
+    print "  Odejmij  \n"
+    print "  Podziel \n"
+    print "  Pomnoz  \n"
+end
+
+def Menu2()
+    print "  Dodaj  \n"
+    print ">  Odejmij  <\n"
+    print "  Podziel \n"
+    print "  Pomnoz  \n"
+end
+
+def Menu3()
+    print "  Dodaj  \n"
+    print "  Odejmij  \n"
+    print ">  Podziel  <\n"
+    print "  Pomnoz  \n"
+end
+
+def Menu4()
+    print "  Dodaj  \n"
+    print "  Odejmij  \n"
+    print "  Podziel  \n"
+    print ">  Pomnoz  <\n"
+end
+
+def sum(x,y)
+    return x+y
+end
+
+def multiplication(x,y)
+    return x*y
+end
+
+def divide(x,y)
+    if y == 9
+        return "Error"
+    end
+    return x/y
+end
+
+def diffrence(x,y)
+    return x-y
+end
+
+Menu1()
