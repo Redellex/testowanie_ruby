956843ebfb5e8f0b242952e8d2ec8fd584e52a5a
Redellex <masterpaw5679966@gmail.com>
2016-01-05 12:32:37 +0100

    Made calculations work fine and new calc fuctions.

--- zadanie_3/lib/main.rb
+++ zadanie_3/lib/main.rb
@@ -5,14 +5,17 @@ $value_y = 0
 $which_value = 0
 $czy_zostal_wybrany_znak = false
 $zatwierdzona_opcja = 0
-
+$result = 0
 
 def get_menus
     return [
         "Dodaj",
         "Odejmij",
         "Podziel",
-        "Pomnoz"
+        "Pomnoz",
+        "Potegowanie",
+        "Silnia",
+        "Reset"
     ]
 end
 
@@ -38,6 +41,39 @@ def draw_menu(position, box)
     }
 end
 
+def sum(x,y)
+    return x+y
+end
+
+def multiplication(x,y)
+    return x*y
+end
+
+def divide(x,y)
+    if y == 0
+        return "Error"
+    end
+    return x/y
+end
+
+def diffrence(x,y)
+    return x-y
+end
+
+def power(x,y)
+    for i in 1..y
+        x = x*x
+    end
+    return x
+end
+
+def silnia(x)
+    if x < 2
+        return 1
+    end
+    return x*silnia(x-1)
+end
+
 def set_box(y1, x1, y2, x2)
     win1 = Curses::Window.new(y1, x1, y2, x2)
     win1.box("|", "-")
@@ -60,15 +96,18 @@ def put_numbers(calc_box, position)
         $value_x = number.to_i
         calc_box.addstr($value_x.to_s)
         $which_value +=1
+        calc_box.refresh
         return position
     when 1
         if $czy_zostal_wybrany_znak
-            calc_box.setpos(1,(position + 1))
+            calc_box.setpos(1,(position + 3))
             number = calc_box.getstr()
             position += number.length + 1
             $value_y = number.to_i
             calc_box.addstr($value_y.to_s)
             $which_value +=1
+            do_calc()
+            calc_box.refresh
         end
         return position
     else 
@@ -78,34 +117,47 @@ end
 
 def show_result(result_box, result)
     result_box.setpos(1,1)
-    result_box.addstr(result.to_s)
+    result_box.addstr($result.to_s)
 end
 
-def wybrana_opcja(y, calc_box)
+def do_calc()
+    case $zatwierdzona_opcja
+    when 1
+        $result = sum($value_x, $value_y)
+    when 2
+        $result = diffrence($value_x, $value_y)
+    when 3
+        $result = divide($value_x, $value_y)
+    when 4
+        $result = multiplication($value_x, $value_y)
+    end
+end
+
+def wybrana_opcja(y, calc_box, number_position)
     case y
     when 1
-        calc_box.setpos(1, 1 + $number_position)
-        calc_box.addstr(' + ')
+        calc_box.setpos(1, 1 + number_position)
+        calc_box.addstr('+')
         $czy_zostal_wybrany_znak = true
     when 2
-        calc_box.setpos(1, 1 + $number_position)
-        calc_box.addstr(' - ')
+        calc_box.setpos(1, 1 + number_position)
+        calc_box.addstr('-')
         $czy_zostal_wybrany_znak = true
     when 3
-        calc_box.setpos(1, 1 + $number_position)
-        calc_box.addstr(' / ')
+        calc_box.setpos(1, 1 + number_position)
+        calc_box.addstr('/')
         $czy_zostal_wybrany_znak = true
     when 4
-        calc_box.setpos(1, 1 + $number_position)
-        calc_box.addstr(' * ')
+        calc_box.setpos(1, 1 + number_position)
+        calc_box.addstr('*')
         $czy_zostal_wybrany_znak = true
     end
+    $zatwierdzona_opcja = y
 end
 
 def initialize_menu(box)
     menupos = 1
     direction = 0
-    result = 0
     number_position = 0
     draw_menu(1, box)
     menu_count = get_menus().length
@@ -117,16 +169,13 @@ def initialize_menu(box)
         box.box("|", "-")
         calc_box.box("|", "-")
         result_box.box("|","-")
-        show_result(result_box, result)
+        show_result(result_box, $result)
         case input
         when Curses::Key::UP
             direction = -1
         when Curses::Key::DOWN
             direction = 1
-        when Curses::Key::ENTER
-            print "Wcisniety enter!"
-            calc_box.setpos(0,1)
-            calc_box.addstr("++++++++++++++++++++++++")
+        when 'x'
             wybrana_opcja(menupos, calc_box, number_position)
         when '0', '1', '2', '3', '4', '5', '6', '7' ,'8', '9'
             direction = 0
@@ -174,33 +223,4 @@ begin
 ensure
     close_box(win1)
     Curses.close_screen
-end
-
-
-
-
-
-
-
-
-
-
-
-def sum(x,y)
-    return x+y
-end
-
-def multiplication(x,y)
-    return x*y
-end
-
-def divide(x,y)
-    if y == 0
-        return "Error"
-    end
-    return x/y
-end
-
-def diffrence(x,y)
-    return x-y
 end
